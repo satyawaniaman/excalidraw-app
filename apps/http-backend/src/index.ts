f71984d275542c0d3fbbc12ff4c8348dc0d459fb
aman satyawani <amansatyawani10@gmail.com>
2025-01-22 19:30:39 +0530

    completed http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,58 +1,98 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from '@repo/backend-common/config';
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
-
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 const app = express();
-
-app.post("/signup", (req, res) => {
-
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
-        res.json({
-            message: "Incorrect inputs"
-        })
-        return;
-    }
-    // db call
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    console.log(parsedData.error);
     res.json({
-        userId: "123"
-    })
-})
-
-app.post("/signin", (req, res) => {
-    const data = SigninSchema.safeParse(req.body);
-    if (!data.success) {
-        res.json({
-            message: "Incorrect inputs"
-        })
-        return;
-    }
-
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  // db call
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: parsedData.data.email,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
     res.json({
-        token
-    })
-})
+      userId: user.id,
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists with this email",
+    });
+  }
+});
 
-app.post("/room", middleware, (req, res) => {
-    const data = CreateRoomSchema.safeParse(req.body);
-    if (!data.success) {
-        res.json({
-            message: "Incorrect inputs"
-        })
-        return;
-    }
-    // db call
-
+app.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
     res.json({
-        roomId: 123
-    })
-})
+      message: "Incorrect inputs",
+    });
+    return;
+  }
 
-app.listen(3001);
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.email,
+      password: parsedData.data.password,
+    },
+  });
+  if (!user) {
+    res.status(403).json({
+      message: "Not authorized",
+    });
+    return;
+  }
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware, async (req, res) => {
+  const parsedData = CreateRoomSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  // @ts-ignore: TODO: Fix this
+  const userId = req.userId;
+  // db call
+  try {
+    const room = await prismaClient.room.create({
+      data: {
+        slug: parsedData.data.name,
+        adminId: userId,
+      },
+    });
+    res.json({
+      roomId: room.id,
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "Room already exists with this name",
+    });
+  }
+});
+
+app.listen(3001, () => {
+  console.log(`Server is running on port 3001`);
+});
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string(),
-    name: z.string()
-})
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string(),
+  photo: z.string().optional(),
+});
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string(),
-})
+  email: z.string().min(3).max(20),
+  password: z.string(),
+});
 
 export const CreateRoomSchema = z.object({
-    name: z.string().min(3).max(20),
-})
+  name: z.string().min(3).max(20),
+});
